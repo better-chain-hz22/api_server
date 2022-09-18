c0c5909379b174ae42d7b3237cf676b385a1e72c
bar haim <barha@il.ibm.com>
2022-09-18 07:10:41 +0200

    build

--- server.py
+++ server.py
@@ -1,11 +1,19 @@
 from helper import MigrosAPI
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
 mgrs = MigrosAPI()
 
+
+app = Flask(__name__, static_url_path='',
+            static_folder='build',
+            template_folder='build')
+
+@app.route("/")
+def hello():
+    return render_template("index.html")
 
 @app.route("/risks_for_order")
 def get_risks_for_order():
@@ -16,11 +24,6 @@ def get_risks_for_order():
 @app.route("/risks_for_ports")
 def get_risks_for_ports():
     return jsonify(mgrs.get_risks_for_ports())
-
-
-@app.route("/")
-def hello_world():
-    return "<p>Hello, Migros!</p>"
 
 
 app.run(debug=True, host='0.0.0.0', port=5000)
